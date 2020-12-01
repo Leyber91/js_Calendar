71e51317048484654a6c5926683fffc737c6f80e
Jan Puig <janpuigcapella@gmail.com>
2020-12-01 17:43:16 +0100

    form works

---
+++ JS/message.js
@@ -0,0 +1,32 @@
+var messageContainer=document.getElementById('messageContainer');
+
+function showMessage(m){
+    var messageBackground=document.createElement('div');
+    messageBackground.classList.add('messageBackground');
+    messageBackground.setAttribute('id', 'messageBackground');
+
+    var message=document.createElement('div');
+    message.classList.add('message');
+    message.innerHTML=m+'<br>';
+
+    var button=document.createElement('button');
+    button.setAttribute('class', 'message__button clickable');
+    button.innerHTML='Ok';
+    button.setAttribute('id', 'messageButton');
+
+    message.appendChild(button);
+    messageBackground.appendChild(message);
+    messageContainer.appendChild(messageBackground);
+
+    button.addEventListener('click', closeMessage);
+    messageBackground.addEventListener('click', closeMessage);
+}
+
+function closeMessage(event){
+    if(event.target.id=='messageBackground' || event.target.id=='messageButton'){
+        document.getElementById('messageBackground').removeEventListener('click', closeMessage);
+        document.getElementById('messageButton').removeEventListener('click', closeMessage);
+
+        messageContainer.innerHTML='';
+    }
+}
