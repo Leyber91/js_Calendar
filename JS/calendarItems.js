e34ea80a54119ce4586896ee1b8a9da011a60513
Jan Puig <janpuigcapella@gmail.com>
2020-12-03 10:00:48 +0100

    main calendar anmation

--- JS/calendarItems.js
+++ JS/calendarItems.js
@@ -1,127 +1,154 @@
 let mainCalendar = document.getElementById("mainCalendarFlexBox");
 
- // adding the class to receive the style
+// adding the class to receive the style
 // We define the actual months and the actual year.
-let todayIs= new Date();
+let todayIs = new Date();
 let d = new Date(); /// this is the key variable to modify each month
 /// set the month with the set month function
-/// IMPORTANT: Set month 
-
+/// IMPORTANT: Set month
 
 let n1 = d.getMonth();
 //n1 = n1 - 10;
 let n2 = d.getFullYear();
 
-
 //console.log(dateMonthYear);
-let getDaysInMonth = function(month,year) {
-    return new Date(year, month, 0).getDate();
-}
-let monthDuration = getDaysInMonth(n1+1, n2); // CAREFULLY MODIFY FOR THE CURRENT MONTH
+let getDaysInMonth = function (month, year) {
+  return new Date(year, month, 0).getDate();
+};
+let monthDuration = getDaysInMonth(n1 + 1, n2); // CAREFULLY MODIFY FOR THE CURRENT MONTH
 
 //FUNCTION TO CREATE THE CALENDAR STRUCTURE
-let generateCalendar = function(){
-    monthDuration = getDaysInMonth(n1+1, n2);
-    mainCalendar.innerHTML="";
-for (let i=1; i <= monthDuration; i++ ){
-    
+let generateCalendar = function () {
+  monthDuration = getDaysInMonth(n1 + 1, n2);
+  mainCalendar.innerHTML = "";
+  for (let i = 1; i <= monthDuration; i++) {
     let divNode = document.createElement("div"); // create de DIV
-    divNode.classList.add("divCalendarItem")
-    let numberContainer=document.createElement("div");
-    let eventContainer=document.createElement("div");
-    let addevent=document.createElement("button");
-    let number=document.createElement("div");
-    addevent.value =n2*10000+(n1+1)*100+i;
-    addevent.classList.add('addButton');
+    divNode.classList.add("divCalendarItem");
+    let numberContainer = document.createElement("div");
+    let eventContainer = document.createElement("div");
+    let addevent = document.createElement("button");
+    let number = document.createElement("div");
+    addevent.value = n2 * 10000 + (n1 + 1) * 100 + i;
+    addevent.classList.add("addButton");
     addevent.classList.add("clickable");
-    addevent.innerHTML='+';
+    addevent.innerHTML = "+";
     // An id must be added to each element
-    eventContainer.id = "day"+ i;
+    eventContainer.id = "day" + i;
     number.innerHTML = i;
     numberContainer.appendChild(number);
     numberContainer.appendChild(addevent);
     divNode.appendChild(numberContainer);
-    
+
     divNode.appendChild(eventContainer);
 
     //is today?
-    if(n1==todayIs.getMonth() && n2==todayIs.getFullYear() &&todayIs.getDate()==i){
-        divNode.classList.add("today")
+    if (
+      n1 == todayIs.getMonth() &&
+      n2 == todayIs.getFullYear() &&
+      todayIs.getDate() == i
+    ) {
+      divNode.classList.add("today");
     }
     mainCalendar.appendChild(divNode);
 
     // define where to take the title
 
-    let month = ["January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+    let month = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
     let dateMonthYear = month[n1] + " " + n2;
     document.getElementById("curentMonthplusYear").innerHTML = dateMonthYear;
     //Add here to add a class to each month.
     //console.log(i);
-
-}}
+  }
+};
 generateCalendar();
-
 
 //let d = new Date();
 //let n1 = d.getMonth();
 //let n2 = d.getFullYear();
-let startOfMonth = function(date) {
-    return new Date(date.getFullYear(), date.getMonth(), 1);
-    }
+let startOfMonth = function (date) {
+  return new Date(date.getFullYear(), date.getMonth(), 1);
+};
 
 let startingWeekDay = startOfMonth(d);
 let d1 = startingWeekDay.getDay();
 //console.log(d1);
 // prepend function in order to add empty cells before the beginning of a month
-let addingEmptyDivsBefore = function(){
-    if (d1 === 0){
-        for (let i = 6; i > 0; i-- ){
-            let divNode = document.createElement("div"); // create de DIV
-            divNode.classList.add("divCalendarItem")
-            divNode.classList.add("divCalendarEmptyItem")
-            // we generate an empty object
-            mainCalendar.prepend(divNode); // append at beginning
-            //console.log(i); 
-        }} else {
-            for (let i = d1-1 ; i > 0 ; i--){
-                let divNode = document.createElement("div"); // create de DIV
-                divNode.classList.add("divCalendarItem")
-                divNode.classList.add("divCalendarEmptyItem")
-                // we generate empty object
-                mainCalendar.prepend(divNode); // append at beginning
-            }
-        }
-}
+let addingEmptyDivsBefore = function () {
+  if (d1 === 0) {
+    for (let i = 6; i > 0; i--) {
+      let divNode = document.createElement("div"); // create de DIV
+      divNode.classList.add("divCalendarItem");
+      divNode.classList.add("divCalendarEmptyItem");
+      // we generate an empty object
+      mainCalendar.prepend(divNode); // append at beginning
+      //console.log(i);
+    }
+  } else {
+    for (let i = d1 - 1; i > 0; i--) {
+      let divNode = document.createElement("div"); // create de DIV
+      divNode.classList.add("divCalendarItem");
+      divNode.classList.add("divCalendarEmptyItem");
+      // we generate empty object
+      mainCalendar.prepend(divNode); // append at beginning
+    }
+  }
+};
 // we execute the function
 addingEmptyDivsBefore();
 
 //for this function we substracted the remanent to the total numbers of divs on a row.
-let parentCalendarChildren = document.getElementById("mainCalendarFlexBox").childElementCount;
+let parentCalendarChildren = document.getElementById("mainCalendarFlexBox")
+  .childElementCount;
 //console.log(parentCalendarChildren) // we use this one to get the number of elements
-let addingEmptyDivAfter = function(){
-    let numOfDivToAdd = 7 - (parentCalendarChildren%7);
-    if(parentCalendarChildren%7==0){
-        numOfDivToAdd=0;
-    }
-    //console.log(numOfDivToAdd);
-    let z = numOfDivToAdd;
+let addingEmptyDivAfter = function () {
+  let numOfDivToAdd = 7 - (parentCalendarChildren % 7);
+  if (parentCalendarChildren % 7 == 0) {
+    numOfDivToAdd = 0;
+  }
+  //console.log(numOfDivToAdd);
+  let z = numOfDivToAdd;
 
-    while (z >0) {
-        let divNode = document.createElement("div"); // create de DIV
-        divNode.classList.add("divCalendarItem")
-        divNode.classList.add("divCalendarEmptyItem")
-        // we generate empty object
-        mainCalendar.append(divNode); // append at beginning
-        z--;
-      }
-}
+  while (z > 0) {
+    let divNode = document.createElement("div"); // create de DIV
+    divNode.classList.add("divCalendarItem");
+    divNode.classList.add("divCalendarEmptyItem");
+    // we generate empty object
+    mainCalendar.append(divNode); // append at beginning
+    z--;
+  }
+};
 addingEmptyDivAfter();
 
 // define where to take the title
 
-let month = ["January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+let month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 let dateMonthYear = month[n1] + " " + n2;
 
 document.getElementById("curentMonthplusYear").innerHTML = dateMonthYear;
-
--- JS/changeMonths.js
+++ JS/changeMonths.js
@@ -2,8 +2,8 @@
 //let btnBefore = document.getElementById("buttonMonthBefore");
 //btnAfter.addEventListener("click", addNextMonth);
 //btnBefore.addEventListener("click",addMonthBefore);
-
 let addNextMonth = function() {
+    let canChange=true;
     // this function will display the month After
     // get the current date that we have
     // get the current date we are
@@ -24,6 +24,7 @@ let addNextMonth = function() {
             }
 
         else if(currentMonth === 12 & currentYear === 2021) { // This condition is placed in order to stopCurrent
+            canChange=false;
             showMessage("Cannot go to 2022");
             currentMonth = 11;
             currentYear = 2021;
@@ -36,15 +37,23 @@ let addNextMonth = function() {
         n1 = currentMonth;
         n2 = currentYear;
         console.log(currentMonth + " " + currentYear);
-        console.log(d)
-        generateCalendar();
-        startingWeekDay = startOfMonth(d);
-        d1 = startingWeekDay.getDay();
-        addingEmptyDivsBefore();
-        parentCalendarChildren = document.getElementById("mainCalendarFlexBox").childElementCount;
-        addingEmptyDivAfter();
-        displayCalendar();
-       // displayCalendar();
+        console.log(!mainCalendar.classList.contains('animate'))
+        if(canChange && !mainCalendar.classList.contains('animate') && !mainCalendar.classList.contains('animateReverse')){
+            mainCalendar.className='animate';
+            setTimeout(() => {
+                generateCalendar();
+                startingWeekDay = startOfMonth(d);
+                d1 = startingWeekDay.getDay();
+                addingEmptyDivsBefore();
+                parentCalendarChildren = mainCalendar.childElementCount;
+                addingEmptyDivAfter();
+                displayCalendar();
+            }, 500);
+            setTimeout(() => {
+                mainCalendar.className='';
+            }, 1001);
+        }
+        
     }
 
 
@@ -56,6 +65,7 @@ let addMonthBefore = function() {
     // get the current date we are
     // change the one we have
     // INFO: n2 get the year and n1 get the month
+    let canChange=true;
 
     let currentMonth = d.getMonth();
     let currentYear = d.getFullYear();
@@ -71,6 +81,7 @@ let addMonthBefore = function() {
         }
 
     else if (currentMonth === -1 && currentYear === 2020){ // This condition is placed in order to stopCurrent
+        canChange=false;
         showMessage("Cannot go before 2020");
         currentMonth = 0;
         currentYear = 2020;
@@ -81,13 +92,22 @@ let addMonthBefore = function() {
     d.setFullYear(currentYear);
     n1 = currentMonth;
     n2 = currentYear;
-    generateCalendar();
-    startingWeekDay = startOfMonth(d);
-    d1 = startingWeekDay.getDay();
-    addingEmptyDivsBefore();
-    parentCalendarChildren = document.getElementById("mainCalendarFlexBox").childElementCount;
-    addingEmptyDivAfter();
-    displayCalendar();
+    if(canChange && !mainCalendar.classList.contains('animateReverse')&& !mainCalendar.classList.contains('animate')){
+        mainCalendar.className='animateReverse';
+        setTimeout(() => {
+            generateCalendar();
+            startingWeekDay = startOfMonth(d);
+            d1 = startingWeekDay.getDay();
+            addingEmptyDivsBefore();
+            parentCalendarChildren = document.getElementById("mainCalendarFlexBox").childElementCount;
+            addingEmptyDivAfter();
+            displayCalendar();
+        }, 500);
+        setTimeout(() => {
+            mainCalendar.className='';
+        }, 1001);
+    }
+    
 }
 
 
